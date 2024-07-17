d5731a4005c4d11f9eea2ed7157113ef767a6189
portlogics-mingyukwak <mg.kwak@portlogics.com>
2024-07-17 17:08:05 +0900

    feat: Glide Data Grid 라이브러리 기초 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,15 @@
-'use client'
+import dynamic from "next/dynamic";
 
-import { ReactGrid, Column, Row, CellChange, TextCell } from "@silevis/reactgrid";
-import "@silevis/reactgrid/styles.css"
-import { useState } from "react";
+const Grid = dynamic(
+    () => {
+        return import("./components/Grid");
+    },
+    { ssr: false }
+);
 
-interface Person {
-  name: string;
-  surname: string;
-}
-
-const getPeople = (): Person[] => [
-  { name: "Thomas", surname: "Goldman" },
-  { name: "Susie", surname: "Quattro" },
-  { name: "", surname: "" }
-];
-
-const getColumns = (): Column[] => [
-  { columnId: "name", width: 150 },
-  { columnId: "surname", width: 150 }
-];
-
-const headerRow: Row = {
-  rowId: "header",
-  cells: [
-    { type: "header", text: "Name" },
-    { type: "header", text: "Surname" }
-  ]
-};
-
-const getRows = (people: Person[]): Row[] => [
-headerRow,
-...people.map<Row>((person, idx) => ({
-  rowId: idx,
-  cells: [
-    { type: "text", text: person.name },
-    { type: "text", text: person.surname }
-  ]
-}))
-];
-
-const applyChangesToPeople = (
-  changes: CellChange<TextCell>[],
-  prevPeople: Person[]
-): Person[] => {
-  changes.forEach((change) => {
-    const personIndex = change.rowId;
-    const fieldName = change.columnId;
-    prevPeople[personIndex][fieldName] = change.newCell.text;
-  });
-  return [...prevPeople];
-};
 
 export default function Home() {
-  const [people, setPeople] = useState<Person[]>(getPeople())
-
-  const rows = getRows(people)
-  const columns = getColumns();
-
-  const handleChanges = (changes: CellChange<TextCell>[]) => {
-    setPeople((prevPeople) => applyChangesToPeople(changes, prevPeople))
-  }
-  
-  return (
-    <ReactGrid rows={rows} columns={columns} onCellsChanged={handleChanges}  />
-  );
+  return <main>
+    <Grid />
+    </main>
 }
